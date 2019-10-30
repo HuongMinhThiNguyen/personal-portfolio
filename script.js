de588474fbb664e48d9f3028f493eed51d673e91
HuongMinhThiNguyen <55386568+HuongMinhThiNguyen@users.noreply.github.com>
2019-10-30 16:13:29 +0100

    updated 30/10

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ $(document).ready(function() {
     $(".hamburger a").on("click", function() {
         // do stuff
         $(".mobile-menu").toggleClass("open");
+        if ($(".mobile-menu").hasClass("open")) {
+            $widthMenu = $(".mobile-menu").width();
+            $("main").css("transform", "translateX($widthMenu)");
+        } else {
+            $("main").css("transform", "");
+
+
+        }
     });
 });
