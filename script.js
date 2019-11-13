139da264029cb87b9625a64563641f1f48e8633b
HuongMinhThiNguyen <55386568+HuongMinhThiNguyen@users.noreply.github.com>
2019-11-13 12:19:39 +0100

    jquery oefening

--- script.js
+++ script.js
@@ -10,12 +10,47 @@ $(document).ready(function() {
             $("main").css("transform", "");
         }
     });
-
+    // make the skill bar bigger when the mouse moves 
     $(".abilities").mouseenter(function() {
-        $('.scale').css('width', '150%');
-        $(".abilities").mouseleave(function() {
-            $('.scale').css('width', '');
-        });
+
+        $('.scale').css('transform', 'scale(1.2)');
 
     });
+
+    $(".abilities").mouseleave(function() {
+
+        $('.scale').css('transform', 'scale(1.0)');
+
+
+    });
+
+    // make the phrase in the header changes its color when the mouse move in/out 
+    $('header').mouseenter(function() {
+        // alert('move')
+        $('header h1').css('color', 'white');
+        $('header h3 span').css('color', 'goldenrod');
+
+    });
+    $('header').mouseleave(function() {
+        // alert('move')
+
+        $('header h1').css('color', 'goldenrod');
+        $('header h3 span').css('color', 'white');
+        // $(this).addClass(".class");
+
+    });
+
+
+    // zoom in/out an image
+
+    $('#header img').hover(
+        function() {
+            $(this).animate({ 'zoom': 1.3 }, 400);
+        },
+        function() {
+            $(this).animate({ 'zoom': 1 }, 400);
+        });
+
+    // $().addClass(className);
+
 });
